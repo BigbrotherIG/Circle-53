f86dd4dc4afe29cf241b513b73f190a85398ec31
BigbrotherIG <itzbigbroda@gmail.com>
2023-11-15 21:58:38 +0100

    It's been a while I check on this project and I'm glad did toady, is time to wrap up this project😊😊👌

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,106 @@
 
     const menuLogo = document.querySelector(".menulogo")
     const navBar = document.querySelector(".navlinks")
+
+        formId = document.querySelector("#form-id")
+        nameId = document.querySelector("#name-id")
+        telephoneId = document.querySelector("#telephone-id")
+        submitBtn = document.querySelector("#submit-btn")
+        inputToggle = document.querySelector(".successInput")
+        msg = document.querySelector(".msg")
     
-    menuLogo.addEventListener("click", openMenu)
+        const logoMenu = document.querySelector(".logomenu")
+        logoMenu.addEventListener("click", openMenu)
+        const logoExit = document.querySelector(".logoexit")
+        logoExit.addEventListener("click", closedMenu)
+        // submitBtn.addEventListener("click", displayError)
 
     function openMenu () {
-        console.log("clicked")
-
-        // menuLogo.classList.toggle("active")
+        logoMenu.classList.toggle("active")
+        logoExit.classList.toggle("active")
         navBar.classList.toggle("active")
     }
 
+    function closedMenu() {
+        logoMenu.classList.toggle("active")
+        logoExit.classList.toggle("active")
+        navBar.classList.toggle("active")
+    }
 
     // document.querySelectorAll(".links").forEach(n => n.addEventListener("click", () => {
     //     navBar.classList.remove("active")
     //     navLinks.classList.remove("active")
     // }))
-
+    
     document.querySelectorAll(".links").forEach(function closeLinks (e) {
-        e.addEventListener("click", closed)
-        // () => {
-        //     navBar.classList.remove("active")
-        // }) 
-
+        e.addEventListener("click", closed)    
         function closed () {
             console.log("click me")
+            openMenu()
             navBar.classList.remove("active")
-        }
+            }
     })
+
+    formId.addEventListener("submit", (e) => {
+        e.preventDefault()
+        
+        if(nameId.value === "" || telephoneId.value === "") {            
+            msg.className = "error"
+            msg.innerText = "Please fill your details"
+                        
+            displayError()                      
+        }
+        else {
+            
+            let para =  document.createElement("input")
+            let paraAppend = document.createTextNode(`${this.nameId.value} your network provider is ${telephoneId.value}.`)
+            
+            // let changeInput = inputToggle
+            // let changeInput = changeInput.setAttribute("type", para )
+            // changeInput = inputToggle.getAttribute("value", para)
+            // inputToggle.setAttribute("type", para)
+            // inputToggle.setAttribute("value", para)
+
+            inputToggle.value  = `${nameId.value} your network provider is ${telephoneId.value}.`
+            console.log(inputToggle.value)
+            
+            msg.className = "success1"
+            msg.innerText = "Successful"
+            setTimeout(()=> {msg.remove()}, 3500)
+            
+            displayError()
+            // Clear fields
+            nameId.value = ""
+            telephoneId.value = ""
+        }
+        
+        telephoneId.addEventListener("input", () => {
+            telephoneId.setCustomValidity("");
+            telephoneId.checkValidity();
+        })
+        
+        telephoneId.addEventListener("invalid", () => {
+            if(telephoneId === "") {
+                telephoneId.setCustomValidity("Enter your phone number!")
+            }
+            else {
+                telephoneId.setCustomValidity("Enter phone number in this format: 234-456-7890");
+            }
+
+        })
+    
+    })
+
+    function displayError () {
+        // setTimeout(()=> msg.remove(), 3500)
+        console.log("click mmm")
+
+        if(nameId.value === "" || telephoneId.value === "") {
+            setTimeout(() => {msg.remove()}, 3500);
+        }
+        else {
+            setTimeout(() => {msg.remove()}, 3500);
+            
+        }
+    }
+    
